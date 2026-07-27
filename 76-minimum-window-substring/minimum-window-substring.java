690fe329589c81a2c59be190c3e4d557cf4d80cf
Mudit Tak <mudittak271@gmail.com>
2026-07-27 20:04:29 +0530

    Time: 3 ms (73.70%) | Memory: 45.4 MB (89.38%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -0,0 +1,71 @@
+class Solution {
+    public String minWindow(String s, String t) {
+
+        if (s.length() < t.length()) {
+            return "";
+        }
+
+        int[] needFreq = new int[128];
+        int[] windowFreq = new int[128];
+
+        // Store frequency of characters required from t
+        for (int i = 0; i < t.length(); i++) {
+            needFreq[t.charAt(i)]++;
+        }
+
+        int left = 0;
+        int count = 0;          // number of satisfied unique characters
+        int required = 0;       // total unique characters in t
+
+        for (int i = 0; i < 128; i++) {
+            if (needFreq[i] > 0) {
+                required++;
+            }
+        }
+
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+
+        // Sliding window
+        for (int right = 0; right < s.length(); right++) {
+
+            char ch = s.charAt(right);
+
+            // Add current character into window
+            windowFreq[ch]++;
+
+            // If this character requirement is satisfied
+            if (needFreq[ch] > 0 && 
+                windowFreq[ch] == needFreq[ch]) {
+                count++;
+            }
+
+            // Window is valid, try shrinking
+            while (count == required) {
+
+                // Update answer
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char leftChar = s.charAt(left);
+
+                // Remove left character
+                windowFreq[leftChar]--;
+
+                // If removing breaks requirement
+                if (needFreq[leftChar] > 0 &&
+                    windowFreq[leftChar] < needFreq[leftChar]) {
+                    count--;
+                }
+
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE 
+                ? "" 
+                : s.substring(start, start + minLen);
+    }
+}
