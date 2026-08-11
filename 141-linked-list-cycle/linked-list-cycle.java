1eeae87f8220bccd03bf6f38235ca993a1ba9a2f
Mudit Tak <mudittak271@gmail.com>
2026-08-11 11:14:28 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (16.27%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -9,13 +9,28 @@
  *     }
  * }
  */
+ // when fast move with 2x 
+// public class Solution {
+//     public boolean hasCycle(ListNode head) {
+//         ListNode slow = head;
+//         ListNode fast = head;
+//         while(fast!=null && fast.next!=null){
+//             slow = slow.next;
+//             fast = fast.next.next;
+//             if(fast==slow) return true;
+//         }
+//         return false;
+        
+//     }
+// }
+// if we want to move fast with 3x speed just some changes
 public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
-        while(fast!=null && fast.next!=null){
+        while(fast!=null && fast.next!=null && fast.next.next!= null){
             slow = slow.next;
-            fast = fast.next.next;
+            fast = fast.next.next.next;
             if(fast==slow) return true;
         }
         return false;
