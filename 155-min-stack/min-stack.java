1aa18d1601f632a34429f9c3dc1dc2b9495c48bc
Mudit Tak <mudittak271@gmail.com>
2026-08-15 12:19:54 +0530

    Time: 34 ms (61.40%) | Memory: 99.8 MB (77.09%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,41 +1,69 @@
 class MinStack {
-    Stack<Long> st;
-    long min;
+    Stack<Integer> st;
+    Stack<Integer> minSt;
     public MinStack() {
         st = new Stack<>();
-        min = Long.MAX_VALUE;
+        minSt = new Stack<>();
     }
     
     public void push(int val) {
-        long value = (long)val;
-        if(st.size()==0) min = value;
-
-        if(value >= min) st.push(value);
-        else{ // val<min, stack me fake value daalo
-            st.push(value + (value-min));
-            min = value;
-        }
+        st.push(val);
+        if(minSt.size()==0 || val < minSt.peek()) minSt.push(val);
+        else minSt.push(minSt.peek());
     }
     
     public void pop() {
-        if(st.peek() < min){ // locha hai, minimum roll back karo
-            min = min + (min - st.peek());
-        }
         st.pop();
+        minSt.pop();
     }
     
     public int top() { // peek
-        long peek = st.peek();
-        if(peek < min){ // locha hai, minimum roll back karo
-            return (int)min;
-        }
-        else return (int)peek;
+        return st.peek();
     }
     
     public int getMin() {
-        return (int)min;
+        return minSt.peek();
     }
 }
+
+// class MinStack {
+//     Stack<Long> st;
+//     long min;
+//     public MinStack() {
+//         st = new Stack<>();
+//         min = Long.MAX_VALUE;
+//     }
+    
+//     public void push(int val) {
+//         long value = (long)val;
+//         if(st.size()==0) min = value;
+
+//         if(value >= min) st.push(value);
+//         else{ // val<min, stack me fake value daalo
+//             st.push(value + (value-min));
+//             min = value;
+//         }
+//     }
+    
+//     public void pop() {
+//         if(st.peek() < min){ // locha hai, minimum roll back karo
+//             min = min + (min - st.peek());
+//         }
+//         st.pop();
+//     }
+    
+//     public int top() { // peek
+//         long peek = st.peek();
+//         if(peek < min){ // locha hai, minimum roll back karo
+//             return (int)min;
+//         }
+//         else return (int)peek;
+//     }
+    
+//     public int getMin() {
+//         return (int)min;
+//     }
+// }
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
