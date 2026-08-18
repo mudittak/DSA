343e4356b7a4aeb6aa346d3b75a3662ee0ca6129
Mudit Tak <mudittak271@gmail.com>
2026-08-18 17:50:55 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (97.19%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -1,39 +1,41 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public void flatten(TreeNode root) {
-        ArrayList<TreeNode> arr = new ArrayList<>();
-        dfs(root,arr);
-        for(int i=0;i<arr.size()-1;i++){
-            TreeNode a = arr.get(i);
-            TreeNode b = arr.get(i+1);
-            a.right = b;
-            a.left = null;
-        }
-        if(arr.size()==0) return;
-        TreeNode last = arr.get(arr.size()-1);
-        last.left = null;
-        last.right = null;
-    }
-
-    private static void dfs(TreeNode root, ArrayList<TreeNode> arr) {
         if(root==null) return;
-        arr.add(root);
-        dfs(root.left,arr);
-        dfs(root.right,arr);
+        TreeNode lst = root.left;
+        TreeNode rst = root.right;
+        root.left = null;
+        // root.right = null;
+        flatten(lst);
+        flatten(rst);
+        root.right = lst;
+        TreeNode last = root; // ye lst ka last element hoga
+        while(last.right!=null) last = last.right;
+        last.right = rst;
+
     }
+}
+// Method 1
+// class Solution {
+//     public void flatten(TreeNode root) {
+//         ArrayList<TreeNode> arr = new ArrayList<>();
+//         dfs(root,arr);
+//         for(int i=0;i<arr.size()-1;i++){
+//             TreeNode a = arr.get(i);
+//             TreeNode b = arr.get(i+1);
+//             a.right = b;
+//             a.left = null;
+//         }
+//         if(arr.size()==0) return;
+//         TreeNode last = arr.get(arr.size()-1);
+//         last.left = null;
+//         last.right = null;
+//     }
+
+//     private static void dfs(TreeNode root, ArrayList<TreeNode> arr) {
+//         if(root==null) return;
+//         arr.add(root);
+//         dfs(root.left,arr);
+//         dfs(root.right,arr);
+//     }
         
- }
+//  }
